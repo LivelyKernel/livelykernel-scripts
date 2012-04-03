3f3ed1b1182084db7cf0154dc849de65f1a78242
Robert Krahn <robert.krahn@gmail.com>
2012-04-03 17:45:17 +0200

    little script for npm publishing livelykernel-scripts itself

--- scripts/helper/test-helper.js
+++ scripts/helper/test-helper.js
@@ -1,3 +1,37 @@
+// genericMock when called with a spyFactory and a nodeunit test object
+// can simulate something like the exec function
+function genericMock(spyFactory, test) {
+    var spies = [], spyIndex = 0;
+
+    function run() {
+        var spy = spies[spyIndex];
+        spyIndex++;
+        if (!spy) {
+            throw new Error('unepectedly trying to call: ' + arguments[0]);
+        }
+        spy.apply(null, arguments);
+    }
+
+    run.expect = function(/*args*/) {
+        for (var i = 0; i < arguments.length; i++) {
+            spies.push(spyFactory(test, arguments[i]));
+        }
+        return run;
+    }
+
+    run.assertAllCalled = function() {
+        spies.forEach(function(spy) {
+            test.equal(spy.called, 1, 'spy called not called: ' + spy);
+        });
+    }
+
+    return run;
+}
+
+//
+// exec
+//
+
 function execEquals(test, actualCommand, expectedCmd, actualCwd, expectedCwd) {
     test.equal(actualCommand, expectedCmd);
     test.equal(actualCwd, expectedCwd);
@@ -7,6 +41,9 @@ function createExecSpy(test, spec) {
     var spy = function(cmd, options, callback) {
         spy.called++;
         execEquals(test, cmd, spec.cmd, options.cwd, spec.cwd);
+        if (!callback) {
+            throw new Error('No callback for ' + spy);
+        }
         callback(spec.code || null, spec.out || "");
     }
     spy.called = 0;
@@ -14,32 +51,46 @@ function createExecSpy(test, spec) {
     return spy;
 }
 
-function execForTest(test) {
-    var spies = [], spyIndex = 0;
+exports.execForTest = genericMock.bind(null, createExecSpy);
 
-    function exec() {
-        var spy = spies[spyIndex];
-        spyIndex++;
-        if (!spy) {
-            throw new Error('unepectedly trying to call exec: ' + arguments[0]);
-        }
-        spy.apply(null, arguments);
-    }
+//
+// fs
+//
 
-    exec.expect = function(/*args*/) {
-        for (var i = 0; i < arguments.length; i++) {
-            spies.push(createExecSpy(test, arguments[i]));
-        }
-        return exec;
-    }
-
-    exec.assertAllCalled = function() {
-        spies.forEach(function(spy) {
-            test.equal(spy.called, 1, 'spy called not called: ' + spy);
-        });
-    }
-
-    return exec;
+function fsWriteEquals(test, actualFile, expectedFile, actualWriteContent, expectedWriteContent) {
+    test.equal(actualFile, expectedFile);
+    test.equal(actualWriteContent, expectedWriteContent);
 }
 
-exports.execForTest = execForTest;
+function createFsWriteSpy(test, spec) {
+    var spy = function(fn, data, callback) {
+        spy.called++;
+        fsWriteEquals(test, fn, spec.file, data, spec.data);
+        callback(spec.code || null);
+    }
+    spy.called = 0;
+    spy.toString = function() { return 'fsWriteSpy<' + spec.file + '>' };
+    return spy;
+}
+
+function fsReadEquals(test, actualFile, expectedFile) {
+    test.equal(actualFile, expectedFile);
+}
+
+function createFsReadSpy(test, spec) {
+    var spy = function(fn, callback) {
+        spy.called++;
+        fsWriteEquals(test, fn, spec.file);
+        callback(spec.code || null, spec.data || '');
+    }
+    spy.called = 0;
+    spy.toString = function() { return 'fsReadSpy<' + spec.file + '>' };
+    return spy;
+}
+
+exports.fsForTest = function(test) {
+    return {
+        writeFile: genericMock(createFsWriteSpy, test),
+        readFile: genericMock(createFsReadSpy, test)
+    }
+}
---
+++ scripts/publish.js
@@ -0,0 +1,197 @@
+/*global process, require, export*/
+
+var path = require('path'),
+    async = require('async'),
+    env = process.env;
+
+/*
+ * This function publishes the livelykernel-scripts project.
+ * It runs the following steps:
+ * 1. update package json
+ * 2. git add && git ci
+ * 3. git tag
+ * 4. git push
+ * 5. git tag push
+ * 6. npm publish
+ *
+ * optionalVersion can either be a version in the format x.y.z (a string)
+ * or null. If null, the existing minor version (z) is increased
+ *
+ */
+function publish(env, optionalVersion, repoDir) {
+    var fs = env.fs,
+        exec = env.exec,
+        packageFile = path.join(repoDir, 'package.json'),
+        packageContent, newPackageContent,
+        newVersion;
+
+    async.series([
+        function readPackageJSON(next) {
+            fs.readFile(packageFile, function(code, data) {
+                packageContent = data;
+                next(code);
+            });
+        },
+        function createNewPackageContent(next) {
+            newPackageContent = newPackageSrc(packageContent);
+            newVersion = extractVersionFrom(newPackageContent);
+            next();
+        },
+        function writePackageJSON(next) {
+            fs.writeFile(packageFile, newPackageContent, next);
+        },
+        function commitPackageJSON(next) {
+            exec('git add package.json && git ci -m "version ' + newVersion + '"',
+                 {cwd: repoDir}, next);
+        },
+        function gitTag(next) {
+            exec('git tag ' + newVersion, {cwd: repoDir}, next);
+        },
+        function gitPush(next) {
+            exec('git push --tags', {cwd: repoDir}, next);
+        },
+        function npmPublish(next) {
+            exec('npm publish', {cwd: repoDir}, next);
+        }
+    ]);
+}
+
+function newPackageSrc(oldPackageContent, optVersion) {
+    var oldVersion = extractVersionFrom(oldPackageContent),
+        newVersion = optVersion || incVersion(oldVersion);
+    return oldPackageContent.replace(oldVersion, newVersion);
+}
+
+function extractVersionFrom(src) {
+    return src.match(/"version":\s*"([^,]+)"/)[1];
+}
+
+function parseVersion(version, nullify) {
+    var regexp = /^([0-9]*)\.?([0-9]*)\.?([0-9]*)$/,
+        match = version.match(regexp),
+        result = match ? [Number(match[1]), Number(match[2]), Number(match[3])] : [];
+    if (nullify) {
+        result[0] = result[0] || 0; result[1] = result[1] || 0; result[2] = result[2] || 0;
+    }
+    return result;
+}
+
+function incVersion(version) {
+    var parsed = parseVersion(version, true);
+    parsed[0] = parsed[0] || 0; parsed[1] = parsed[1] || 0; parsed[2] = parsed[2] || 0;
+    parsed[2]++;
+    return parsed.join('.');
+}
+
+function isValidVersion(version) {
+    var parsed = parseVersion(version);
+    return parsed && typeof parsed[0] == 'number';
+}
+
+if (!env.LK_SCRIPT_TEST_RUN) {
+
+    var args           = require('./helper/args'),
+        fs             = require('fs'),
+        exec           = require('child_process').exec;
+
+    // -=-=-=-=-=-=-=-=-=-=-
+    // script options
+    // -=-=-=-=-=-=-=-=-=-=-
+    var options = args.options([
+        ['-h', '--help', 'Show this help'],
+        ['-t', '--tag TAG', 'New version tag, in the form of x.y.z.']],
+        {},
+        "publish a new version of livelykernel-sripts");
+
+    if (options.defined('tag') && !isValidVersion(options.tag)) {
+        console.error('tag ' + options.tag + ' has not the form x.y.z');
+        process.exit(1);
+    }
+
+    // "scripts/.."
+    var scriptDir = path.dirname(__dirname);
+    publish({fs: fs, exec: exec}, options.tag, scriptDir);
+
+} else {
+
+    /*
+     * = = = = =
+     * Test code
+     * = = = = =
+     */
+
+    var testHelper = require('./helper/test-helper');
+
+    module.exports = {
+        setUp: function (callback) {
+            this.packageJSONSrc = "{\n" +
+                "  \"author\": \"The Lively Kernel Team <lively-kernel@hpi.uni-potsdam.de>\",\n" +
+                "  \"name\": \"livelykernel-scripts\",\n" +
+                "  \"description\": \"Foo\",\n" +
+                "  \"version\": \"0.0.6\",\n" +
+                "  \"homepage\": \"http://lively-kernel.org\",\n" +
+                "  \"repository\": {\n" +
+                "    \"type\": \"git\",\n" +
+                "    \"url\": \"git@github.com:rksm/livelykernel-scripts.git\"\n" +
+                "  },\n" +
+                "  \"engines\": {\"node\": \"~0.6.7\"},\n" +
+                "  \"bin\" : {\n" +
+                "    \"lk\" : \"./bin/lk\"\n" +
+                "  },\n" +
+                "  \"dependencies\": {}\n" +
+                "}";
+            this.packageJSONSrcWithMinorInc = this.packageJSONSrc.replace('0.0.6', '0.0.7');
+            this.scriptDir = 'foo/bar';
+            this.packageFile = this.scriptDir + '/package.json';
+            callback();
+        },
+
+        testExtractVersion: function(test) {
+            test.equals(extractVersionFrom(this.packageJSONSrc), '0.0.6');
+            test.done();
+        },
+
+        testIncVersion: function(test) {
+            test.equals(incVersion('0.0.6'), '0.0.7');
+            test.equals(incVersion('0.1'), '0.1.1');
+            test.equals(incVersion('1.1'), '1.1.1');
+            test.equals(incVersion('1'), '1.0.1');
+            test.done();
+        },
+
+        testUpdatedPackageContent: function(test) {
+            test.equals(newPackageSrc(this.packageJSONSrc), this.packageJSONSrcWithMinorInc);
+            test.equals(newPackageSrc(this.packageJSONSrc, '3.2.1'),
+                        this.packageJSONSrc.replace('0.0.6', '3.2.1'));
+            test.done();
+        },
+
+        testValidateNewVersion: function(test) {
+            test.ok(isValidVersion('0.1.2'), '0.1.2');
+            test.ok(isValidVersion('0.1'), '0.1');
+            test.ok(isValidVersion('033'), '033');
+            test.ok(!isValidVersion('csfsd'), 'csfsd');
+            test.ok(!isValidVersion('23x2'), '23x2');
+            test.done();
+        },
+
+        testPublish: function (test) {
+            var fs = testHelper.fsForTest(test);
+            fs.readFile.expect(
+                {file: this.packageFile, data: this.packageJSONSrc});
+            fs.writeFile.expect(
+                {file: this.packageFile, data: this.packageJSONSrcWithMinorInc});
+            var exec = testHelper.execForTest(test).expect(
+                {cmd: 'git add package.json && git ci -m "version 0.0.7"', cwd: 'foo/bar'},
+                {cmd: 'git tag 0.0.7', cwd: 'foo/bar'},
+                {cmd: 'git push --tags', cwd: 'foo/bar'},
+                {cmd: 'npm publish', cwd: 'foo/bar'});
+
+            publish({fs: fs, exec: exec}, null, this.scriptDir);
+            fs.readFile.assertAllCalled();
+            fs.writeFile.assertAllCalled();
+            exec.assertAllCalled();
+            test.done();
+        }
+    }
+}
