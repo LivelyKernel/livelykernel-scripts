39cdcf524faabbe99671f0a28405d6582796e54f
Robert Krahn <robert.krahn@gmail.com>
2012-04-22 03:38:50 +0200

    getting started with a workspace server

--- minimal_server/serve.js
+++ minimal_server/serve.js
@@ -2,6 +2,7 @@
 
 var express = require('express'),
     spawn = require('child_process').spawn,
+    exec = require('child_process').exec,
     shell = require('./../scripts/helper/shell'),
     defaultBrowser = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
     defaultArgs =  ["--no-process-singleton-dialog",
@@ -19,23 +20,48 @@ var express = require('express'),
 
 var args = process.argv.slice(2),
     port = args[0] && parseInt(args[0], 10),
-    lkDir = args[1] || __dirname + '/../';
+    lkDir = args[1],
+    verbose = args.indexOf('--verbose') >= 0 || args.indexOf('-v') >= 0;
 
 /*
  * http interface
  */
-function setupServer(testHandler) {
+function setupServer(testHandler, workspaceHandler) {
     var app = express.createServer();
-    app.use(express.logger());
+    if (verbose) app.use(express.logger());
     app.use("/", express["static"](lkDir));
     app.use(express.errorHandler({dumpExceptions: true, showStack: true}));
     app.use(express.bodyParser());
 
+    testHandler.registerWith(app);
+    workspaceHandler.registerWith(app);
+
+    return app;
+}
+
+function getJSONRoute(app, handler, path, method) {
+    app.get(path, function (req, res) {
+        handler[method](function(json) { res.send(json); });
+    });
+}
+
+/*
+ * Controller that process result responses from LK client
+ */
+function TestHandler() {}
+
+var testResults;
+(TestHandler.resetTestData = function() {
+    testResults = {};
+})();
+
+TestHandler.prototype.registerWith = function(app) {
+    var handler = this;
     app.post('/test-result/:id', function(req, res) {
         var id = req.params.id,
             result = req.body.testResults;
         try {
-            result = testHandler.postResult(id, result);
+            result = this.postResult(id, result);
         } catch(e) {
             result = {result: String(e), error: true, requestedData: req.body};
         }
@@ -43,27 +69,15 @@ function setupServer(testHandler) {
     });
 
     app.get('/test-result/:id', function(req, res) {
-        res.send(testHandler.getResult(req.params.id));
+        res.send(handler.getResult(req.params.id));
     });
 
     app.get('/test-result', function (req, res) {
-        res.send(testHandler.listResults());
+        res.send(handler.listResults());
     });
-
-    return app;
 }
 
-
-function TestHandler() {}
-
-var testResults;
-TestHandler.resetTestData = function() {
-    testResults = {};
-};
-TestHandler.resetTestData();
-
 // results
-
 TestHandler.prototype.postResult = function(id, result) {
     console.log('Getting result for test run ' + id);
     testResults[id] = {testRunId: id, state: 'done', result: result};
@@ -78,11 +92,79 @@ TestHandler.prototype.listResults = function(req) {
     return {result: JSON.stringify(testResults)};
 };
 
+
+/*
+ * Handler that provides an interface for inspecting / modifying
+ * a local LK workspace
+ */
+function WorkspaceHandler(modules, lkDir) {
+    this.modules = modules;
+    this.lkDir = lkDir;
+}
+
+WorkspaceHandler.prototype.registerWith = function(app) {
+    getJSONRoute(app, this, '/workspace/workspace-dir', 'getLKDir');
+    getJSONRoute(app, this, '/workspace/branches', 'getBranches');
+    var handler = this;
+    app.get('/workspace/log/:ref/:howMany', function(req, res) {
+        handler.getLog(req.params.ref, req.params.howMany, function(json) { res.send(json); });
+    });
+}
+
+WorkspaceHandler.prototype.git = function(cmd, whenDone) {
+    this.modules.exec("git " + cmd, {cwd: this.lkDir}, whenDone)
+}
+
+// curl localhost:9001/workspace/workspace-dir | prettify_json.rb
+WorkspaceHandler.prototype.getLKDir = function(whenDone) {
+    whenDone({path: this.lkDir});
+}
+
+// curl localhost:9001/workspace/branches | prettify_json.rb
+WorkspaceHandler.prototype.getBranches = function(whenDone) {
+    this.git("branch -a --color=never", function(code, out, err) {
+        if (code) { whenDone({error: code, out: out, err: err}); return }
+        var lines = out.split('\n'),
+            names = [], current;
+        lines.forEach(function(line, i) {
+            var match = line.match(/(\*?)\s*(.*)/);
+            if (!match) return;
+            if (match[1] && match[1].length > 0) current = names.length;
+            names.push(match[2]);
+        });
+        whenDone({names: names, current: current});
+    })
+}
+
+// curl localhost:9001/workspace/log/HEAD/10 | prettify_json.rb
+WorkspaceHandler.prototype.getLog = function(fromRef, howMany, whenDone) {
+    var cmd = "log --graph "
+            + "--pretty=format:'%h -%d %s (%cr) <%an>' "
+            + "--abbrev-commit "
+            + "--date=relative "
+            + (fromRef || "")
+            + (howMany ? " -" + howMany : "");
+    this.git(cmd, function(code, out, err) {
+        if (code) { whenDone({error: code, out: out, err: err}); return }
+        var lines = out.split('\n');
+        whenDone({lines: lines});
+    })
+}
+
+
+
+
 /*
  * startup or export
  */
 if (port && !isNaN(port)) {
-    setupServer(new TestHandler()).listen(port);
+    if (!lkDir) {
+        throw new Error('Cannot start server without LK core directory '
+                       + 'for serving files from!');
+    }
+
+    setupServer(new TestHandler(), new WorkspaceHandler({exec: exec}, lkDir)).listen(port);
 } else {
     exports.TestHandler = TestHandler;
+    exports.WorkspaceHandler = WorkspaceHandler;
 }
--- minimal_server/serve_test.js
+++ minimal_server/serve_test.js
@@ -1,12 +1,15 @@
 /*global exports, require, JSON*/
 
 // continously run with:
-// nodemon --exec qunit --code ./minimal_server/serve.js --tests ./minimal_server/serve_test.js
+// nodemon node_modules/nodeunit/bin/nodeunit minimal_server/serve_test.js
 
-console.log(__dirname);
-
-var TestHandler = require('./serve').TestHandler,
+var server = require('./serve'),
+    testHelper = require('../scripts/helper/test-helper'),
+    TestHandler = server.TestHandler,
+    WorkspaceHandler = server.WorkspaceHandler,
     testSuite = {};
+
+
 /*
  * requesting test run
  */
@@ -38,6 +41,63 @@ testSuite.StatusHandlerTest = {
         test.deepEqual(report, {testRunId: 1, state: 'done', result: "all ok"}, JSON.stringify(report));
         test.done();
     },
+}
+
+
+/*
+ * Workspace tests
+ */
+var lkDir = "foo/bar/", modules;
+testSuite.WorkspaceHandlerTest = {
+
+    setUp: function(run) {
+        modules = {};
+        handler = new WorkspaceHandler(modules, lkDir);
+        run();
+    },
+
+    tearDown: function(run) { run(); },
+
+    "get lk core dir": function(test) {
+        handler.getLKDir(function(json) {
+            test.equals(json.path, lkDir);
+            test.done();
+        });
+    },
+
+    "get branches": function(test) {
+        modules.exec = testHelper.execForTest(test).expect(
+            {cmd: 'git branch -a --color=never', cwd: lkDir,
+             out: "  foo\n* master\n  baz\n  remotes/origin/master\n  remotes/origin/bla"});
+        var expected = {
+            names: ["foo", "master", "baz", "remotes/origin/master", "remotes/origin/bla"],
+            current: 1
+        };
+        handler.getBranches(function(json) {
+            test.deepEqual(json, expected);
+            modules.exec.assertAllCalled(test);
+            test.done();
+        });
+    },
+
+    "get log": function(test) {
+        var log = "* bdece40 - (HEAD, origin/master, master) yoy (2 hours ago) <tuetata>\n"
+                + "* 9e0ab4c - abc (7 weeks ago) <Foo bar>\n"
+                + "* 0ab7f5c - def (7 weeks ago) <Baz Bla>\n"
+                + "*   6fe108d - Merge pull request #28 from xxx (7 weeks ago) <Foo bar>\n"
+                + "|\  \n"
+                + "| * 7027758 - (origin/yyy, xxx) blupf bla (8 weeks ago) <baz napf>";
+        modules.exec = testHelper.execForTest(test).expect(
+            {cmd: "git log --graph --pretty=format:'%h -%d %s (%cr) <%an>' "
+                  + "--abbrev-commit --date=relative "
+                  + "HEAD -10", cwd: lkDir, out: log});
+        var expected = {lines: log.split('\n')};
+        handler.getLog('HEAD', 10, function(json) {
+            test.deepEqual(json, expected);
+            modules.exec.assertAllCalled(test);
+            test.done();
+        });
+    }
 }
 
 exports.testSuite = testSuite;
