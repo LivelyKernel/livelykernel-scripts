036d61bae65436b8f9c48263431f7d38cda20b79
Robert Krahn <robert.krahn@gmail.com>
2012-05-05 23:42:54 +0200

    removing workspace support, it is now part of life_star

--- minimal_server/serve.js
+++ minimal_server/serve.js
@@ -2,7 +2,6 @@
 
 var express = require('express'),
     spawn = require('child_process').spawn,
-    exec = require('child_process').exec,
     shell = require('./../scripts/helper/shell'),
     defaultBrowser = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
     defaultArgs =  ["--no-process-singleton-dialog",
@@ -30,7 +29,7 @@ var args = process.argv.slice(2),
 /*
  * http interface
  */
-function setupServer(testHandler, workspaceHandler) {
+function setupServer(testHandler) {
     var app = express.createServer();
     if (verbose) app.use(express.logger());
     app.use("/", express["static"](lkDir));
@@ -38,16 +37,15 @@ function setupServer(testHandler, workspaceHandler) {
     app.use(express.bodyParser());
 
     testHandler.registerWith(app);
-    workspaceHandler.registerWith(app);
 
     return app;
 }
 
-function getJSONRoute(app, handler, path, method) {
-    app.get(path, function (req, res) {
-        handler[method](function(json) { res.send(json); });
-    });
-}
+// function getJSONRoute(app, handler, path, method) {
+//     app.get(path, function (req, res) {
+//         handler[method](function(json) { res.send(json); });
+//     });
+// }
 
 /*
  * Controller that process result responses from LK client
@@ -96,68 +94,6 @@ TestHandler.prototype.listResults = function(req) {
     return {result: JSON.stringify(testResults)};
 };
 
-
-/*
- * Handler that provides an interface for inspecting / modifying
- * a local LK workspace
- */
-function WorkspaceHandler(modules, lkDir) {
-    this.modules = modules;
-    this.lkDir = lkDir;
-}
-
-WorkspaceHandler.prototype.registerWith = function(app) {
-    getJSONRoute(app, this, '/workspace/workspace-dir', 'getLKDir');
-    getJSONRoute(app, this, '/workspace/branches', 'getBranches');
-    var handler = this;
-    app.get('/workspace/log/:ref/:howMany', function(req, res) {
-        handler.getLog(req.params.ref, req.params.howMany, function(json) { res.send(json); });
-    });
-}
-
-WorkspaceHandler.prototype.git = function(cmd, whenDone) {
-    this.modules.exec("git " + cmd, {cwd: this.lkDir}, whenDone)
-}
-
-// curl localhost:9001/workspace/workspace-dir | prettify_json.rb
-WorkspaceHandler.prototype.getLKDir = function(whenDone) {
-    whenDone({path: this.lkDir});
-}
-
-// curl localhost:9001/workspace/branches | prettify_json.rb
-WorkspaceHandler.prototype.getBranches = function(whenDone) {
-    this.git("branch -a --color=never", function(code, out, err) {
-        if (code) { whenDone({error: code, out: out, err: err}); return }
-        var lines = out.split('\n'),
-            names = [], current;
-        lines.forEach(function(line, i) {
-            var match = line.match(/(\*?)\s*(.*)/);
-            if (!match) return;
-            if (match[1] && match[1].length > 0) current = names.length;
-            names.push(match[2]);
-        });
-        whenDone({names: names, current: current});
-    })
-}
-
-// curl localhost:9001/workspace/log/HEAD/10 | prettify_json.rb
-WorkspaceHandler.prototype.getLog = function(fromRef, howMany, whenDone) {
-    var cmd = "log --graph "
-            + "--pretty=format:'%h -%d %s (%cr) <%an>' "
-            + "--abbrev-commit "
-            + "--date=relative "
-            + (fromRef || "")
-            + (howMany ? " -" + howMany : "");
-    this.git(cmd, function(code, out, err) {
-        if (code) { whenDone({error: code, out: out, err: err}); return }
-        var lines = out.split('\n');
-        whenDone({lines: lines});
-    })
-}
-
-
-
-
 /*
  * startup or export
  */
@@ -169,8 +105,7 @@ if (require.main === module) {
     if (port && isNaN(port)) {
         throw new Error('port must be a number');
     }
-    setupServer(new TestHandler(), new WorkspaceHandler({exec: exec}, lkDir)).listen(port);
+    setupServer(new TestHandler()).listen(port);
 } else {
     exports.TestHandler = TestHandler;
-    exports.WorkspaceHandler = WorkspaceHandler;
 }
--- minimal_server/serve_test.js
+++ minimal_server/serve_test.js
@@ -4,9 +4,7 @@
 // nodemon node_modules/nodeunit/bin/nodeunit minimal_server/serve_test.js
 
 var server = require('./serve'),
-    testHelper = require('../scripts/helper/test-helper'),
     TestHandler = server.TestHandler,
-    WorkspaceHandler = server.WorkspaceHandler,
     testSuite = {};
 
 
@@ -40,64 +38,8 @@ testSuite.StatusHandlerTest = {
         var report = handler.getResult(reportRequest.body.testRunId);
         test.deepEqual(report, {testRunId: 1, state: 'done', result: "all ok"}, JSON.stringify(report));
         test.done();
-    },
-}
-
-
-/*
- * Workspace tests
- */
-var lkDir = "foo/bar/", modules;
-testSuite.WorkspaceHandlerTest = {
-
-    setUp: function(run) {
-        modules = {};
-        handler = new WorkspaceHandler(modules, lkDir);
-        run();
-    },
-
-    tearDown: function(run) { run(); },
-
-    "get lk core dir": function(test) {
-        handler.getLKDir(function(json) {
-            test.equals(json.path, lkDir);
-            test.done();
-        });
-    },
-
-    "get branches": function(test) {
-        modules.exec = testHelper.execForTest(test).expect(
-            {cmd: 'git branch -a --color=never', cwd: lkDir,
-             out: "  foo\n* master\n  baz\n  remotes/origin/master\n  remotes/origin/bla"});
-        var expected = {
-            names: ["foo", "master", "baz", "remotes/origin/master", "remotes/origin/bla"],
-            current: 1
-        };
-        handler.getBranches(function(json) {
-            test.deepEqual(json, expected);
-            modules.exec.assertAllCalled(test);
-            test.done();
-        });
-    },
-
-    "get log": function(test) {
-        var log = "* bdece40 - (HEAD, origin/master, master) yoy (2 hours ago) <tuetata>\n"
-                + "* 9e0ab4c - abc (7 weeks ago) <Foo bar>\n"
-                + "* 0ab7f5c - def (7 weeks ago) <Baz Bla>\n"
-                + "*   6fe108d - Merge pull request #28 from xxx (7 weeks ago) <Foo bar>\n"
-                + "|\  \n"
-                + "| * 7027758 - (origin/yyy, xxx) blupf bla (8 weeks ago) <baz napf>";
-        modules.exec = testHelper.execForTest(test).expect(
-            {cmd: "git log --graph --pretty=format:'%h -%d %s (%cr) <%an>' "
-                  + "--abbrev-commit --date=relative "
-                  + "HEAD -10", cwd: lkDir, out: log});
-        var expected = {lines: log.split('\n')};
-        handler.getLog('HEAD', 10, function(json) {
-            test.deepEqual(json, expected);
-            modules.exec.assertAllCalled(test);
-            test.done();
-        });
     }
 }
+
 
 exports.testSuite = testSuite;
