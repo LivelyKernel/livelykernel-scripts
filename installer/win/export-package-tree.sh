5a56404b62f7d3cd4de7b9d563fa61479116fd1e
Robert Krahn <robert.krahn@gmail.com>
2012-08-21 15:36:39 -0700

    adding windows installer creator files and infos

---
+++ installer/win/export-package-tree.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# copy only the necessary into a new dir used by the msi creator
+# ./export-package-tree.sh ~/Public/win7-vm/livelyinstall-test ~/Public/win7-vm/export
+
+SOURCE_DIR=$1
+TARGET_DIR=$2
+
+[ -d $TARGET_DIR ] && rm -rfd $TARGET_DIR
+mkdir -p $TARGET_DIR
+
+# archive, verbose, copy-links
+# exclude:
+# git repos emacs auto-saves, workspace, installer package source
+    # --include="livelykernel-scripts/.git" \
+rsync -av \
+    --exclude="*.git" \
+    --exclude="*~" --exclude=".#*" \
+    --exclude="livelykernel-scripts/workspace/" \
+    --exclude="livelykernel-scripts/bin/lk" \
+    --exclude="npm-debug.log" \
+    --exclude="*.aip" --exclude "livelykernel-scripts-create-msi-cache" \
+    $SOURCE_DIR/ $TARGET_DIR
+
+echo "Package source is in $TARGET_DIR"
