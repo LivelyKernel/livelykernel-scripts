2cb880ffaaa9056c0882a5463d417cb27460b6d4
Robert Krahn <robert.krahn@gmail.com>
2012-08-22 00:29:51 -0700

    eben better partsbin script

--- scripts/lk-partsbin.js
+++ scripts/lk-partsbin.js
@@ -21,9 +21,13 @@ var options = args.options([
 // the real thing
 // -=-=-=-=-=-=-=-=-=-=-
 var dir = options.dir || env.PARTSBIN_DIR,
-    isInstalled = fs.existsSync(dir);
+    isInstalled = fs.existsSync(dir),
+    cmd = "svn",
+    spawnArgs = isInstalled ? ['up'] : ['co', env.PARTSBIN_SVN_URL, dir],
+    spawnOptions = isInstalled ? {cwd: dir, stdio: "inherit"} : {stdio: "inherit"},
+    msg = isInstalled ? 'Updating PartsBin (' + dir + ')' :
+    'Downloading PartsBin to ' + dir + '. This may take a minute...';
 
-var proc = isInstalled ?
-    spawn('svn', ['up'], {cwd: dir, stdio: "inherit"}) :
-    spawn('svn', ['co', env.PARTSBIN_SVN_URL, dir], {stdio: "inherit"});
+console.log(msg);
+var proc = spawn(cmd, spawnArgs, spawnOptions);
 proc.on('exit', function(code) { process.exit(code); });
