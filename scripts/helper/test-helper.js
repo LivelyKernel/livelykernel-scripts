a9f1b21fe53e5f3fbadc14d0646296f3341d790e
Robert Krahn <robert.krahn@gmail.com>
2012-09-07 17:12:29 -0700

    cleaning up spy code and adding spy for shelljs

--- scripts/helper/test-helper.js
+++ scripts/helper/test-helper.js
@@ -1,32 +1,48 @@
+var util = require("util");
+
 // genericMock when called with a spyFactory and a nodeunit test object
 // can simulate something like the exec function
-function genericMock(spyFactory, test) {
-    var spies = [], spyIndex = 0;
+function genericSpy(bodyFunc) {
+    var invovationsToRun = [],
+        ignoreUnexpected = false,
+        callCount = 0;
+
+    function createInvocation(spec) {
+        function invocation() {
+            var actualArgs = [];
+            for (var i = 0; i < arguments.length; i++) { actualArgs.push(arguments[i]); }
+            callCount++;
+            return bodyFunc.apply(invocation, [spec].concat(actualArgs));
+        }
+        invocation.toString = function() { return bodyFunc.name + '<' + util.inspect(spec) + '>' };
+        return invocation;
+    }
 
     function run() {
-        var spy = spies[spyIndex];
-        spyIndex++;
+        var spy = invovationsToRun.shift();
         if (!spy) {
+            if (ignoreUnexpected) return;
             throw new Error('unepectedly trying to call: ' + arguments[0]);
         }
         spy.apply(null, arguments);
     }
 
-    run.expect = function(/*args*/) {
+    run.ignoreUnexpected = function() { ignoreUnexpected = true }
+
+    run.expect = function(/*specs*/) {
         for (var i = 0; i < arguments.length; i++) {
-            spies.push(spyFactory(test, arguments[i]));
+            invovationsToRun.push(createInvocation(arguments[i]));
         }
         return run;
     }
 
     run.assertAllCalled = function() {
-        spies.forEach(function(spy) {
-            test.equal(spy.called, 1, 'spy called not called: ' + spy);
-        });
+        if (invovationsToRun.length === 0) return;
+        throw new Error('spies not called: ' + invovationsToRun);
     }
 
     run.toString = function() {
-        return 'Mock< spies:' + spies.join('\n') + '>';
+        return 'Mock< invovationsToRun:' + invovationsToRun.join('\n') + '>';
     }
 
     return run;
@@ -35,66 +51,43 @@ function genericMock(spyFactory, test) {
 //
 // exec
 //
-
-function execEquals(test, actualCommand, expectedCmd, actualCwd, expectedCwd) {
-    test.equal(actualCommand, expectedCmd);
-    test.equal(actualCwd, expectedCwd);
-}
-
-function createExecSpy(test, spec) {
-    var spy = function(cmd, options, callback) {
-        spy.called++;
-        execEquals(test, cmd, spec.cmd, options.cwd, spec.cwd);
+exports.execForTest = function(test) {
+    return genericSpy(function execSpy(spec, cmd, options, callback) {
+        test.equal(cmd, spec.cmd);
+        test.equal(options.cwd, spec.cwd);
         if (!callback) {
-            throw new Error('No callback for ' + spy);
+            throw new Error('No callback for ' + this);
         }
         callback(spec.code || null, spec.out || "");
-    }
-    spy.called = 0;
-    spy.toString = function() { return 'execSpy<' + spec.cmd + '>' };
-    return spy;
+    });
 }
-
-exports.execForTest = genericMock.bind(null, createExecSpy);
 
 //
 // fs
 //
-
-function fsWriteEquals(test, actualFile, expectedFile, actualWriteContent, expectedWriteContent) {
-    test.equal(actualFile, expectedFile);
-    test.equal(actualWriteContent, expectedWriteContent);
-}
-
-function createFsWriteSpy(test, spec) {
-    var spy = function(fn, data, callback) {
-        spy.called++;
-        fsWriteEquals(test, fn, spec.file, data, spec.data);
-        callback(spec.code || null);
-    }
-    spy.called = 0;
-    spy.toString = function() { return 'fsWriteSpy<' + spec.file + '>' };
-    return spy;
-}
-
-function fsReadEquals(test, actualFile, expectedFile) {
-    test.equal(actualFile, expectedFile);
-}
-
-function createFsReadSpy(test, spec) {
-    var spy = function(fn, callback) {
-        spy.called++;
-        fsWriteEquals(test, fn, spec.file);
-        callback(spec.code || null, spec.data || '');
-    }
-    spy.called = 0;
-    spy.toString = function() { return 'fsReadSpy<' + spec.file + '>' };
-    return spy;
-}
-
 exports.fsForTest = function(test) {
     return {
-        writeFile: genericMock(createFsWriteSpy, test),
-        readFile: genericMock(createFsReadSpy, test)
+        writeFile: genericSpy(function writeSpy(spec, fn, data, callback) {
+            test.equal(fn, spec.file);
+            test.equal(data, spec.data);
+            callback(spec.code || null);
+        }),
+        readFile: genericSpy(function readSpy(spec, fn, callback) {
+            test.equal(fn, spec.file);
+            callback(spec.code || null, spec.data || '');
+        })
     }
+}
+
+exports.shelljs = function(test) {
+    var spies = {
+        echo: genericSpy(function echoSpy(expectedString, actualString) {
+            test.equal(actualString, expectedString, "echo not OK"); })
+    };
+    spies.beGlobal = function() {
+        Object.keys(spies).forEach(function(name) {
+            global[name] = spies[name];
+        });
+    }
+    return spies;
 }
