ff6ea1b7a6d9b812ae697b3e998322f42e81f0b4
Robert Krahn <robert.krahn@gmail.com>
2013-02-13 23:04:02 -0800

    update lk build-libs to include ace

--- scripts/lk-build-libs.js
+++ scripts/lk-build-libs.js
@@ -5,14 +5,16 @@
  *
  */
 
-var args  = require('./helper/args'),
-    fs    = require('fs'),
-    async = require('async'),
-    path  = require('path'),
-    http  = require('http'),
-    https = require('https'),
-    url   = require('url'),
-    env   = process.env;
+var args   = require('./helper/args'),
+    fs     = require('fs'),
+    exec   = require('child_process').exec,
+    async  = require('async'),
+    path   = require('path'),
+    http   = require('http'),
+    https  = require('https'),
+    url    = require('url'),
+    dryice = require('dryice'),
+    env    = process.env;
 
 
 // -=-=-=-=-=-=-=-=-=-=-
@@ -87,7 +89,39 @@ function resetComment(next) {
     next();
 }
 
-var urls = {
+function gitClone(options, next) {
+    var dir = path.join(env.LK_SCRIPTS_ROOT, options.to);
+    async.waterfall([
+        function(next) { fs.exists(dir, function(exists) { next(null, exists); }); },
+        function(exists, next) {
+            if (!exists) { next(); return; }
+            console.log("%s exists, removing it... ", dir);
+            exec('rm -rfd ' + dir, function(code, out, err) {
+                if (code) { console.error('rm: %s', err); }
+                console.log('done');
+                next(code);
+            })
+        }
+    ], function(err) {
+        console.log("git clone %s -> %s... ", options.url, dir);
+        exec("git clone " + options.url + " " + dir, function(code, out, err) {
+            if (code) { console.error("git clone: " + err); }
+            else { console.log('done.'); }
+            next(code);
+        })
+    })
+}
+
+function dryicePackage(spec, next) {
+    spec.dest = path.join(options.lkDir, spec.dest);
+    console.log("dryice %s -> %s", spec.source.root, spec.dest);
+    dryice.copy(spec);
+    next(null);
+}
+
+function createCoreLibs(next) {
+    console.log("Packaging core libs...");
+    var urls = {
         jquery: {
             src: "http://code.jquery.com/jquery-1.8.2.js",
             minified: "http://code.jquery.com/jquery-1.8.2.min.js"
@@ -101,30 +135,49 @@ var urls = {
             minified: "https://raw.github.com/kriskowal/es5-shim/v2.0.5/es5-shim.min.js"
         }
     },
-    downloadsMinified = [
-        download.bind(global, urls.jquery.minified),
-        downloadAndModify.bind(global, urls.jqueryBounds.src, urls.jqueryBounds.minify),
-        download.bind(global, urls.es5Shim.minified)
-    ],
-    downloadsDebug = [
-        download.bind(global, urls.jquery.src),
-        download.bind(global, urls.jqueryBounds.src),
-        download.bind(global, urls.es5Shim.src)
-    ],
-    localLibs = [copyLocal.bind(global, "resources/pre-lib/requestAnimationFrame.js"),
-                 copyLocal.bind(global, "resources/pre-lib/IE-fixes.js")],
+        downloadsMinified = [
+            download.bind(global, urls.jquery.minified),
+            downloadAndModify.bind(global, urls.jqueryBounds.src, urls.jqueryBounds.minify),
+            download.bind(global, urls.es5Shim.minified)
+        ],
+        downloadsDebug = [
+            download.bind(global, urls.jquery.src),
+            download.bind(global, urls.jqueryBounds.src),
+            download.bind(global, urls.es5Shim.src)
+        ],
+        localLibs = [copyLocal.bind(global, "resources/pre-lib/requestAnimationFrame.js"),
+                     copyLocal.bind(global, "resources/pre-lib/IE-fixes.js")],
 
-    comment = '';
+        comment = '';
 
-async.series([
-    function(next) {
-        async.series(
-            [resetComment].concat(downloadsMinified).concat(localLibs),
-            write.bind(global, next, 'core/lib/lively-libs.js'))
-    },
-    function(next) {
-        async.series(
-            [resetComment].concat(downloadsDebug).concat(localLibs),
-            write.bind(global, next, 'core/lib/lively-libs-debug.js'))
-    }
-]);
+    async.series([
+        function(next) {
+            async.series(
+                [resetComment].concat(downloadsMinified).concat(localLibs),
+                write.bind(global, next, 'core/lib/lively-libs.js'))
+        },
+        function(next) {
+            async.series(
+                [resetComment].concat(downloadsDebug).concat(localLibs),
+                write.bind(global, next, 'core/lib/lively-libs-debug.js'))
+        }
+    ], function(err) { next && next() });
+}
+
+function createExternalLibs(next) {
+    // package up ace
+    console.log("Packaging external libs...");
+    async.series([
+        gitClone.bind(global, {
+            url: "https://github.com/ajaxorg/ace-builds.git",
+            to: "resources/pre-lib/ace-builds"}),
+        dryicePackage.bind(global, {
+            source: {root: "resources/pre-lib/ace-builds/src-min-noconflict"},
+            dest: "core/lib/lively-ace.min.js"}),
+        dryicePackage.bind(global, {
+            source: {root: "resources/pre-lib/ace-builds/src-noconflict"},
+            dest: "core/lib/lively-ace.js"})
+    ], function(err) { next && next() });
+}
+
+async.series([createCoreLibs, createExternalLibs]);
